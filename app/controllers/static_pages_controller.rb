42a70b0009cc2ed629eb5f4c94ea6e9d1b17fe66
addod19 <addodaniellarbi@gmail.com>
2019-10-17 19:00:42 +0000

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,17 @@
 class StaticPagesController < ApplicationController
   def home
+    #code
   end
 
   def help
+    #code
+  end
+
+  def about
+    #code
+  end
+
+  def contact
+    #code
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-
   get 'static_pages/help'
-
+  get 'static_pages/about'
+  get 'static_pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'static_pages#home'
 end
