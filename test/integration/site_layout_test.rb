eac206250c95ff19276a4c472b1ba2e92371b58f
addod19 <addodaniellarbi@gmail.com>
2019-10-24 08:07:12 +0000

    finished layouts and test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -12,6 +12,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
     # get contact_path
     # assert_select "title", full_title("Contact")
   end
