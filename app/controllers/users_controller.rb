ae55373f9c61bbf1dd9fbb008db130cf9a586b47
addod19 <addodaniellarbi@gmail.com>
2019-10-28 14:49:56 +0000

    debugger setup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def new; end
+  def new
+    debugger
+  end
 
   def show
     @user = User.find(params[:id])
+    debugger
   end
 end
