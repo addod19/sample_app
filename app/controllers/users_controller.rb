48d0cf8ad644d4e9ec2051fca0ee902c640b9de1
addod19 <addodaniellarbi@gmail.com>
2019-10-28 20:48:32 +0000

    signup error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     #debugger
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
