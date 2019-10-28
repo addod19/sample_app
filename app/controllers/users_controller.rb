210fd7137ff7cbbeffda9bb009b63fc95b85e597
addod19 <addodaniellarbi@gmail.com>
2019-10-28 14:27:48 +0000

    added show view and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@
 
 class UsersController < ApplicationController
   def new; end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
+  #get 'users/new'
+  
   root 'static_pages#home'
   get 'static_pages/home'
   get  '/help', to: 'static_pages#help'
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
 
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
