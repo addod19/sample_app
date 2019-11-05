651c6edc7c8870035be8a8a3c1acde8e5d12b389
addod19 <addodaniellarbi@gmail.com>
2019-11-05 18:30:38 +0000

    the log in method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
+    user = User.find_by(email: params[:sessions][:email].downcase)
+    if user && user.authenticate(params[:sessions][:password])
+      session[:user_id] = user.id
+      flash[:success] = 'Log in successful, you are welcome!'
+      #log_in user
       redirect_to user
     else
       flash.now[:danger] = "Invalid email/password combination!"
