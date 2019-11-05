6f911c55a905974a185d36d3a6cdb5378ca06e50
addod19 <addodaniellarbi@gmail.com>
2019-11-05 14:58:21 +0000

    finding and authenticating a user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:sessions][:email].downcase)
+    if user && user.authenticate(params[:sessions][:password])
   end
 
   def destroy
