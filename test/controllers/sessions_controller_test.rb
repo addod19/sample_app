229d22e6c9fb021269297f351e19d4f8c2d71727
addod19 <addodaniellarbi@gmail.com>
2019-11-05 14:16:33 +0000

    session controller and login view

---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get login_path
+    #get sessions_new_url
+    assert_response :success
+  end
+
+end
