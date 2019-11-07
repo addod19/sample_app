2026a9ad1dc48adebaebd694b14e090179e9ed29
addod19 <addodaniellarbi@gmail.com>
2019-11-07 12:21:32 +0000

    testng layout changes

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -4,13 +4,29 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
-  test "login with invalid information" do
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test 'login with invalid information' do
     get login_path
     assert_template 'sessions/new'
-    post login_path, params: { session: { email: "", password: "" } }
+    post login_path, params: { sessions: { email: '', password: '' } }
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+
+  test 'login with valid information' do
+    get login_path
+    post login_path, params: { sessions: { email: @user.email, password: 'password'} }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
