218cf5dadf8ab4999b93f576f4da949491f5d7b6
addod19 <addodaniellarbi@gmail.com>
2019-11-12 14:01:44 +0000

    test incomplete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,11 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
+    # ?user = User.find_by(email: params[:session][:email].downcase)
+    # if ?user && ?user.authenticate(params[:session][:password])
+    #   log_in ?user
+    #   params[:session][:remember_me] == '1' ? remember(?user) : forget(?user)
+    #   redirect_to ?user
     user = User.find_by(email: params[:sessions][:email].downcase)
     if user && user.authenticate(params[:sessions][:password])
       session[:user_id] = user.id
