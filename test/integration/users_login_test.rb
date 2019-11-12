68c8cc0f7049c3e41b5b71dcef524502863d281d
addod19 <addodaniellarbi@gmail.com>
2019-11-12 09:49:50 +0000

    Two subtle bugs

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -46,6 +46,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not logged_in?
     assert_redirected_to root_url
+    # Simulate a user clicking logout in a second window.
+    delete logout_path
     follow_redirect!
     assert_select 'a[href=?]', login_path
     assert_select 'a[href=?]', logout_path, count: 0
