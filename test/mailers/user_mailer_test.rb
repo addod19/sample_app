93323f6b296d415cf259513dd0788d473fa488d3
addod19 <addodaniellarbi@gmail.com>
2019-11-13 16:52:43 +0000

    email preview and email test

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,12 +1,21 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-  test "account_activation" do
-    mail = UserMailer.account_activation
+  test 'account_activation' do
+    user = users(:michael)
+    user.activation_token = User.new_token
+    mail = UserMailer.account_activation(user)
     assert_equal "Account activation", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_equal [user.email], mail.to
+    assert_equal ["noreply@example.com"], mail.from
+    assert_match user.name,               mail.body.encoded
+    assert_match user.activation_token,   mail.body.encoded
+    assert_match CGI.escape(user.email),  mail.body.encoded
+    # mail = UserMailer.account_activation
+    # assert_equal "Account activation", mail.subject
+    # assert_equal ["to@example.org"], mail.to
+    # assert_equal ["from@example.com"], mail.from
+    # assert_match "Hi", mail.body.encoded
   end
 
   test "password_reset" do
