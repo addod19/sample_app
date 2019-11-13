3b33209b498613c73f8cd92f16c6603ec7bce3fa
addod19 <addodaniellarbi@gmail.com>
2019-11-13 14:17:49 +0000

    user destroy test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -44,7 +44,8 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
                                               email: @user.email } }
     assert_not flash.empty?
     assert_redirected_to login_url
-  endtest 'should not allow the admin attribute to be edited via the web' do
+  end
+  test 'should not allow the admin attribute to be edited via the web' do
     log_in_as(@other_user)
     assert_not @other_user.admin?
     patch user_path(@other_user), params: {
@@ -53,4 +54,20 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
                                             admin: FILL_IN } }
     assert_not @other_user.FILL_IN.admin?
   end
+
+  test 'should redirect destroy when not logged in' do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect destroy when logged in as a non-admin' do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
+
 end
