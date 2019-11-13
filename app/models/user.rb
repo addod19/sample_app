f48dd3af111c99499d721a7dedd912f545b6bd5c
addod19 <addodaniellarbi@gmail.com>
2019-11-13 16:28:25 +0000

    mailer template

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,7 +45,8 @@ class User < ApplicationRecord
 
   # Converts email to all lower-case.
   def downcase_email
-    self.email = email.downcase
+    email.downcase!
+    #self.email = email.downcase
   end
 
   # Creates and assigns the activation token and digest.
