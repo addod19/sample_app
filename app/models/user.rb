e63615c5a84152d043796914156a596ab6e0b10c
addod19 <addodaniellarbi@gmail.com>
2019-11-14 10:42:58 +0000

    password resets create action

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email # { email.downcase! }
   before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
@@ -44,9 +44,19 @@ class User < ApplicationRecord
 
   # Activates an account.
   def activate
-    update_columns(activated: FILL_IN, activated_at: FILL_IN)
-    # update_attribute(:activated,    true)
-    # update_attribute(:activated_at, Time.zone.now)
+    #update_columns(activated: FILL_IN, activated_at: FILL_IN)
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
 
   # Sends activation email.
