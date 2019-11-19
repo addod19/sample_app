936d9bdc58b48edd70d83971f7ab12662f9a165a
addod19 <addodaniellarbi@gmail.com>
2019-11-19 14:33:38 +0000

    Dependent destroy

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -29,4 +29,8 @@ class MicropostTest < ActiveSupport::TestCase
     @micropost.content = 'a' * 141
     assert_not @micropost.valid?
   end
+
+  test 'order should be most recent first' do
+    assert_equal microposts(:most_recent), Micropost.first
+  end
 end
