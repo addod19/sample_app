596f3f382185e8adc73722a2fcb56d757b0fe9b4
addod19 <addodaniellarbi@gmail.com>
2019-11-19 22:58:32 +0000

    image resizing

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,4 +1,6 @@
 class PictureUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
