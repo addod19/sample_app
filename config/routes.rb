86de2599fc1a099ca5e3329e7326204070d62cb1
addod19 <addodaniellarbi@gmail.com>
2019-12-13 12:14:57 +0000

    stats and a follow form

--- config/routes.rb
+++ config/routes.rb
@@ -17,9 +17,14 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
