f23727af8b9cb1025a8ff7dcb61aca2855b9e23e
addod19 <addodaniellarbi@gmail.com>
2019-12-13 15:22:46 +0000

    A working follow button with Ajax

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,12 +4,20 @@ class RelationshipsController < ApplicationController
   def create
     user = User.find(params[:followed_id])
     current_user.follow(user)
-    redirect_to user
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
+    # redirect_to user
   end
 
   def destroy
     user = Relationship.find(params[:id]).followed
     current_user.unfollow(user)
-    redirect_to user
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
+    # redirect_to user
   end
 end
