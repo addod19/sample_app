0c754f0c7d8f846d9e250ff36d309e8371210177
addod19 <addodaniellarbi@gmail.com>
2019-12-13 15:37:52 +0000

    following test not passing yet

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -6,6 +6,7 @@ class FollowingTest < ActionDispatch::IntegrationTest
   # end
   def setup
     @user = users(:michael)
+    @other = users(:lana)
     log_in_as(@user)
   end
 
@@ -24,6 +25,34 @@ class FollowingTest < ActionDispatch::IntegrationTest
     assert_match @user.followers.count.to_s, response.body
     @user.followers.each do |user|
       assert_select "a[href=?]", user_path(user)
+    end
+  end
+
+  test 'should follow a user the standard way' do
+    assert_difference '@user.following.count', 1 do
+      post relationships_path, params: { followed_id: @other.id }
+    end
+  end
+
+  test 'should follow a user with Ajax' do
+    assert_difference '@user.following.count', 1 do
+      post relationships_path, xhr: true, params: { followed_id: @other.id }
+    end
+  end
+
+  test 'should unfollow a user the standard way' do
+    @user.follow(@other)
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    assert_difference '@user.following.count', -1 do
+      delete relationship_path(relationship)
+    end
+  end
+
+  test 'should unfollow a user with Ajax' do
+    @user.follow(@other)
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    assert_difference '@user.following.count', -1 do
+      delete relationship_path(relationship), xhr: true
     end
   end
 end
